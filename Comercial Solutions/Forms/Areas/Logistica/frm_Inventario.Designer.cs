bf048dd2f92584094948abd5ffdc2e2b689c4598
Edgar Carrera <ditocarrera@hotmail.com>
2014-05-13 16:57:20 -0600

    Resolución de formularios, nuevas consultas

---
+++ Comercial Solutions/Forms/Areas/Logistica/frm_Inventario.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Comercial_Solutions.Forms.Areas.Logistica
+{
+    partial class frm_inventario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab_consulta = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Insertar = new System.Windows.Forms.TabPage();
+            this.btn_cargar = new System.Windows.Forms.Button();
+            this.btn_insertar = new System.Windows.Forms.Button();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.txt_cantidad = new System.Windows.Forms.TextBox();
+            this.txt_precioventa = new System.Windows.Forms.TextBox();
+            this.txt_preciocompra = new System.Windows.Forms.TextBox();
+            this.txt_medida = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tab_consulta.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Insertar.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab_consulta
+            // 
+            this.tab_consulta.Controls.Add(this.tabPage1);
+            this.tab_consulta.Controls.Add(this.Insertar);
+            this.tab_consulta.Controls.Add(this.tabPage2);
+            this.tab_consulta.Location = new System.Drawing.Point(24, 137);
+            this.tab_consulta.Name = "tab_consulta";
+            this.tab_consulta.SelectedIndex = 0;
+            this.tab_consulta.Size = new System.Drawing.Size(667, 340);
+            this.tab_consulta.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(659, 314);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Consultar";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Insertar
+            // 
+            this.Insertar.Controls.Add(this.label5);
+            this.Insertar.Controls.Add(this.label4);
+            this.Insertar.Controls.Add(this.label3);
+            this.Insertar.Controls.Add(this.label2);
+            this.Insertar.Controls.Add(this.label1);
+            this.Insertar.Controls.Add(this.txt_medida);
+            this.Insertar.Controls.Add(this.txt_preciocompra);
+            this.Insertar.Controls.Add(this.txt_precioventa);
+            this.Insertar.Controls.Add(this.txt_cantidad);
+            this.Insertar.Controls.Add(this.txt_nombre);
+            this.Insertar.Controls.Add(this.btn_insertar);
+            this.Insertar.Location = new System.Drawing.Point(4, 22);
+            this.Insertar.Name = "Insertar";
+            this.Insertar.Padding = new System.Windows.Forms.Padding(3);
+            this.Insertar.Size = new System.Drawing.Size(659, 314);
+            this.Insertar.TabIndex = 1;
+            this.Insertar.Text = "Agregar";
+            this.Insertar.UseVisualStyleBackColor = true;
+            // 
+            // btn_cargar
+            // 
+            this.btn_cargar.Location = new System.Drawing.Point(712, 189);
+            this.btn_cargar.Name = "btn_cargar";
+            this.btn_cargar.Size = new System.Drawing.Size(75, 23);
+            this.btn_cargar.TabIndex = 1;
+            this.btn_cargar.Text = "Cargar";
+            this.btn_cargar.UseVisualStyleBackColor = true;
+            this.btn_cargar.Click += new System.EventHandler(this.btn_cargar_Click);
+            // 
+            // btn_insertar
+            // 
+            this.btn_insertar.Location = new System.Drawing.Point(567, 15);
+            this.btn_insertar.Name = "btn_insertar";
+            this.btn_insertar.Size = new System.Drawing.Size(75, 23);
+            this.btn_insertar.TabIndex = 0;
+            this.btn_insertar.Text = "button1";
+            this.btn_insertar.UseVisualStyleBackColor = true;
+            this.btn_insertar.Click += new System.EventHandler(this.btn_insertar_Click);
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(130, 33);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(100, 20);
+            this.txt_nombre.TabIndex = 1;
+            // 
+            // txt_cantidad
+            // 
+            this.txt_cantidad.Location = new System.Drawing.Point(130, 60);
+            this.txt_cantidad.Name = "txt_cantidad";
+            this.txt_cantidad.Size = new System.Drawing.Size(100, 20);
+            this.txt_cantidad.TabIndex = 2;
+            // 
+            // txt_precioventa
+            // 
+            this.txt_precioventa.Location = new System.Drawing.Point(130, 87);
+            this.txt_precioventa.Name = "txt_precioventa";
+            this.txt_precioventa.Size = new System.Drawing.Size(100, 20);
+            this.txt_precioventa.TabIndex = 3;
+            // 
+            // txt_preciocompra
+            // 
+            this.txt_preciocompra.Location = new System.Drawing.Point(130, 114);
+            this.txt_preciocompra.Name = "txt_preciocompra";
+            this.txt_preciocompra.Size = new System.Drawing.Size(100, 20);
+            this.txt_preciocompra.TabIndex = 4;
+            // 
+            // txt_medida
+            // 
+            this.txt_medida.Location = new System.Drawing.Point(130, 141);
+            this.txt_medida.Name = "txt_medida";
+            this.txt_medida.Size = new System.Drawing.Size(100, 20);
+            this.txt_medida.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "cantidad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Precio venta";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "precio compra";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(68, 144);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "medida";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.comboBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(659, 314);
+            this.tabPage2.TabIndex = 2;
+            this.tabPage2.Text = "combo";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(72, 61);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.Click += new System.EventHandler(this.comboBox1_Click);
+            // 
+            // frm_inventario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 502);
+            this.Controls.Add(this.btn_cargar);
+            this.Controls.Add(this.tab_consulta);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm_inventario";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Inventario_Load);
+            this.tab_consulta.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Insertar.ResumeLayout(false);
+            this.Insertar.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tab_consulta;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage Insertar;
+        private System.Windows.Forms.Button btn_cargar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_medida;
+        private System.Windows.Forms.TextBox txt_preciocompra;
+        private System.Windows.Forms.TextBox txt_precioventa;
+        private System.Windows.Forms.TextBox txt_cantidad;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.Button btn_insertar;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ComboBox comboBox1;
+
+    }
+}
---
+++ Comercial Solutions/Forms/Areas/Logistica/frm_Inventario.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Comercial_Solutions.Clases;
+using i3nRiqJSON;
+
+namespace Comercial_Solutions.Forms.Areas.Logistica
+{
+    public partial class frm_inventario : Form
+    {
+        int X = 0;
+        int Y = 0;
+        public frm_inventario()
+        {
+            X = Propp.X;
+            Y = Propp.Y;
+            InitializeComponent();
+        }
+        i3nRiqJson x = new i3nRiqJson();
+        private void Inventario_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(X, Y);
+            this.Location = new Point(250, 56);
+        }
+
+        private void btn_cargar_Click(object sender, EventArgs e)
+        {
+        
+            string query = "Select * from tbm_producto_finalizado";
+            dataGridView1.DataSource = ((x.consulta_DataGridView(query)));
+        }
+
+        private void btn_insertar_Click(object sender, EventArgs e)
+        {
+            string tabla = "tbm_producto_finalizado";
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+           
+            dict.Add("medida_producto", txt_medida.Text);
+            dict.Add("precio_costo", txt_preciocompra.Text);
+            dict.Add("precio_venta", txt_precioventa.Text); 
+            dict.Add("cantidad_producto_finalizado", txt_cantidad.Text);
+            dict.Add("tx_nombre", txt_nombre.Text);
+            //
+           // i3nRiqJson x = new i3nRiqJson();
+            x.insertar("1", tabla, dict);
+            MessageBox.Show("Trae " + i3nRiqJson.RespuestaConexion.ToString());
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void comboBox1_Click(object sender, EventArgs e)
+        {
+            string query = "Select medida_producto from tbm_producto_finalizado";
+            comboBox1.DataSource = (x.consulta_ComboBox(query));
+            comboBox1.DisplayMember = "medida_producto";
+
+        }
+    }
+}
